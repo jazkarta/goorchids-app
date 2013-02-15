a2d47829c9758299f1e3a247517889ecc99b3c2d
Alec Mitchell <alecpm@gmail.com>
2013-02-15 08:56:56 -0800

    Update settings to use custom default bucket name and local url config.

--- goorchids/settings.py
+++ goorchids/settings.py
@@ -1,3 +1,11 @@
 from gobotany.settings import *
 
+ROOT_URLCONF = 'goorchids.core.urls'
+
 INSTALLED_APPS.insert(0, 'goorchids.core')
+
+if 'test' in sys.argv:
+    pass
+else:
+    AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME',
+                                             'goorchids')
