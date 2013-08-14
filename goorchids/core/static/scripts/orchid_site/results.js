8591c268077cf6dfd146e9c672a3d1715133c48d
Alec Mitchell <alecpm@gmail.com>
2013-08-13 18:36:38 -0700

    Add handlebars helper to convert newlines into br and apply to species popup factoid field.  Fixes #67

--- goorchids/core/static/scripts/orchid_site/results.js
+++ goorchids/core/static/scripts/orchid_site/results.js
@@ -10,7 +10,11 @@ define([
         e.preventDefault();
         $('body,html').animate({scrollTop: $('#question-nav').offset().top}, 200);
     });
-
+    // Register handlebars helper function to turn newlines into <br>
+    Handlebars.registerHelper("linebreaksbr", function (text) {
+        text = text.toString();
+        return text.replace(/(\r\n|\n|\r)/gm, '<br />');
+    });
     // Make Ember selectboxes use select2
     // (from http://stackoverflow.com/questions/14910785/ember-select-with-custom-select-plugin-how-to-bind-the-two)
     Ember.Select.reopen({
