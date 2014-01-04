4a3b907c224dd79ad581ee439871fabbeb3ff9cf
David Glick <david@glicksoftware.com>
2014-01-04 10:47:26 -0800

    avoid redirect which loses the fragment in IE

--- goorchids/site/views.py
+++ goorchids/site/views.py
@@ -46,7 +46,7 @@ def location_suggestions_view(request):
 def redirect_to_simple_key_by_location(request):
     """Redirect to the simple key with a particular location selected."""
     location = request.GET.get('state_distribution', '')
-    return redirect('/simple/monocots/orchid-monocots#state_distribution=%s' % location)
+    return redirect('/simple/monocots/orchid-monocots/#state_distribution=%s' % location)
 
 
 def _plant_name_suggestions(query, querytype='istartswith', site_search=False):
