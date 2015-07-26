934b7e6efbd2f34270a38bd70da8b79974ef540d
Alec Mitchell <alecpm@gmail.com>
2015-07-26 01:36:02 -0400

    Allow referencing static files using /static/ or {{STATIC_URL}} in flat pages.

---
+++ goorchids/site/templatetags/static_url.py
@@ -0,0 +1,17 @@
+import re
+from django import template
+from django.conf import settings
+from django.template.defaultfilters import stringfilter
+
+register = template.Library()
+STATIC_RE = re.compile(r'(\{\{\s*STATIC_URL\s*\}\})|(/static/)')
+
+
+@register.filter
+@stringfilter
+def static_url(value):
+    """Searches for {{ STATIC_URL }} or /static/ and replaces it with the
+       STATIC_URL from settings.py"""
+    value = STATIC_RE.sub(settings.STATIC_URL, value)
+    return value
+static_url.is_safe = True
