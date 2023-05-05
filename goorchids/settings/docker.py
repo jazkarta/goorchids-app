f723f6e44ad6f3238a5580d92f7f93c81aa6f7e4
Chiruzzi Marco <chiruzzi.marco@gmail.com>
2023-05-05 15:34:13 +0200

    Fix production like environment redirect issue outside Heroku

--- goorchids/settings/docker.py
+++ goorchids/settings/docker.py
@@ -15,3 +15,8 @@ if 'test' not in sys.argv:
     HAYSTACK_CONNECTIONS['default']['ADMIN_URL'] = 'http://solr:8983/solr/#/~cores'
 
 ALLOWED_HOSTS = ["*"]
+
+# Remove the django.middleware.security.SecurityMiddleware
+# added by gobotany to allow for HTTPS redirection on Heroku
+if "django.middleware.security.SecurityMiddleware" in MIDDLEWARE:
+    del MIDDLEWARE[MIDDLEWARE.index("django.middleware.security.SecurityMiddleware")]
